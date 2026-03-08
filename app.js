e63ccc3510a0bf56738fea32edc614c12dc6dde4
Harwin <sandhuharwinkaur@gmail.com>
2026-03-08 22:56:29 +1100

    files added

--- app.js
+++ app.js
@@ -22,25 +22,45 @@ app.use(express.static(__dirname + "/public"));
 //     res.send("<h1>Hello Server Dev</h1> <hr/> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>");
 // });
 
+  //Home page controller
+
 app.get("/", (req, res) => {
     console.log("request received");
     res.sendFile(__dirname +"/src/html/index.html");
 })
 
-// app.get("/get-user", (req, res) => {
-//     res.json({
-//         fName: "Harwin",
-//         lName: "Kaur",
-//     })
-// })
 
-//this one will give api only
-
-app.get("/api/v1/get-user", (req, res) => {
+app.get("/get-user", (req, res) => {
     res.json({
         fName: "Harwin",
         lName: "Kaur",
     })
+})
+
+//this one will give api only
+
+
+    /* app.get("/api/v1/get-user", (req, res) => {
+        res.json({
+            fName: "Harwin",
+            lName: "Kaur",
+        })
+    })  */
+
+
+// user registeration controller
+
+app.get("/register", (req, res) => {
+    console.log(req.query);
+    console.log("request received login");
+    res.sendFile(__dirname +"/src/html/register.html");
+})
+
+//User Login controller
+
+app.get("/login", (req, res) => {
+    console.log("request received");
+    res.sendFile(__dirname +"/src/html/login.html");
 })
 
 app.listen(PORT, (error) => {
